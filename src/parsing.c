ac106293f922b6070a0e7aacaea39fcb239a0ef9
David Stumpf <david-dsf@gmx.de>
2026-04-22 21:29:04 +0200

    alter lstadd_back to include NULL check for content; add tokenize functionality to build token linked list on basis of existing libft functions

--- src/parsing.c
+++ src/parsing.c
@@ -6,7 +6,7 @@
 /*   By: dstumpf <dstumpf@student.42vienna.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/20 14:14:19 by dstumpf           #+#    #+#             */
-/*   Updated: 2026/04/20 15:30:14 by dstumpf          ###   ########.fr       */
+/*   Updated: 2026/04/22 13:53:45 by dstumpf          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,11 @@ void	parse_input(t_data *dat)
 	t_token *tokens;
 	//t_compound_tokens *compound_tokens;
 	//t_compound_commands *compound_commands;
+	//t_ast *ast;
 
 	tokens = tokenize(dat);
 	print_tokens(tokens);
 	//compound_tokens = compound_group(tokens);
 	//expand(dat, compound_tokens);
-	//compound_commands = merge_compound_tokens(dat, compound_tokens);
+	//make_parsetree = parse(dat, compound_tokens);
 }
