aac7599f07f5cc85c0852913357ff02cc1fefa3a
David Stumpf <david-dsf@gmx.de>
2026-04-24 18:05:37 +0200

    start tokenizing functions; basic metachar tokens are handled, though space tab and newline are tokens right now too this should  be removed. also word token is not yet handled

--- src/tokenize.c
+++ src/tokenize.c
@@ -6,7 +6,7 @@
 /*   By: dstumpf <dstumpf@student.42vienna.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/20 15:15:50 by dstumpf           #+#    #+#             */
-/*   Updated: 2026/04/23 16:38:38 by dstumpf          ###   ########.fr       */
+/*   Updated: 2026/04/24 18:03:53 by dstumpf          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -72,42 +72,103 @@ t_node	*new_token_node(enum e_token type, enum e_quote quote, char *lexeme)
 	return (node);
 }
 
+static char	char_in_str(char c, const char *str)
+{
+	size_t		i;
+
+	i = -1;
+	while (str[++i])
+	{
+		if (str[i] == c)
+			return (c);
+	}
+	return ('\0');
+}
+
+static char	get_metachar(char c)
+{
+	const char	*metachars;
+
+	metachars = "|&;()<> \t\n";
+	return (char_in_str(c, metachars));
+}
+
+static char	is_double_metachar(char c)
+{
+	const char	*double_metachars;
+
+	double_metachars = "|&<>";
+	return (char_in_str(c, double_metachars));
+}
+
+//note for tokens: &&, ||, >> or << the actual numeric enum value/type variable inside the token
+//refers to 2 * token + 1 which is passed to new_token_node
+static t_node	*meta_token(char **input, char metachar)
+{
+	if (is_double_metachar(**input) && (*input)[1] == metachar)
+	{
+		(*input) = (*input) + 2;
+		return (new_token_node(metachar	* 2 + 1, NONE, NULL));
+	}
+	(*input)++;
+	return (new_token_node(metachar, NONE, NULL));
+}
+
+static t_node	*word_token(char **input)
+{
+	(void)input;
+	while (**input && !get_metachar(**input))
+		(*input)++;
+	return (new_token_node(WORD, NONE, "test_word"));
+}
+
+t_node	*find_next_token(char **input)
+{
+	char	metachar;
+
+	metachar = get_metachar(**input);
+	if (metachar)
+		return (meta_token(input, metachar));
+	return (word_token(input));
+}
+
 t_list	*tokenize(t_data *dat)
 {
-	(void)dat;
-	t_list *lst = ft_lstnew();
+	char	*input;
+	t_node	*new_node;
+	t_list	*lst;
+
+	input = dat->input;
+	lst = ft_lstnew();
 	if (!lst)
-		exit(1);
-
-	t_node *tok = new_token_node(WORD, NONE, "hello"); 
-	if (!tok)
 		cleanup(lst);
-	ft_lstadd_back(lst, tok);
-
-	tok = new_token_node(PIPE, NONE, "you"); 
-	if (!tok)
-		cleanup(lst);
-	ft_lstadd_back(lst, tok);
-
+	while (*input)
+	{
+		new_node = find_next_token(&input);
+		if (!new_node)
+			cleanup(lst);
+		ft_lstadd_back(lst, new_node);
+	}
 	return (lst);
 }
 
 //remove this funciton since its not relevant just for debugging
 #include "stdio.h"
+
 void	print_token(void *content)
 {
-	char *token_map[] = {
-	"WORD",
-	"PIPE",
-	"AND",
-	"OR",
-	"REDIR_INFILE",
-	"REDIR_OUTFILE",
-	"REDIR_HEREDOC",
-	"REDIR_APPEND",
-	"LEFT_PARA",
-	"RIGHT_PARAN"
-	};
+	char *token_map[253];
+	token_map[WORD] = "WORD";
+	token_map[PIPE] = "PIPE";
+	token_map[AND] = "AND";
+	token_map[OR] = "OR";
+	token_map[REDIR_INFILE] = "REDIR_INFILE";
+	token_map[REDIR_OUTFILE] = "REDIR_OUTFILE";
+	token_map[REDIR_HEREDOC] = "REDIR_HEREDOC";
+	token_map[REDIR_APPEND] = "REDIR_APPEND";
+	token_map[LEFT_PARA] = "LEFT_PARA";
+	token_map[RIGHT_PARA] = "RIGHT_PARA";
+	token_map[' '] = "SPACE";
 	t_token *tok = (t_token *)content;
 	printf("Type: %s  ", token_map[tok->type]);
 	if (tok->type == WORD)
