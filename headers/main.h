7cfdd2c654210b039d28a14aebf88b9980579ab1
Kian Najmechi <knajmech@c3r2p3.42vienna.com>
2026-05-07 09:50:14 +0200

    Working env hash_map

--- headers/main.h
+++ headers/main.h
@@ -6,15 +6,16 @@
 /*   By: dstumpf <dstumpf@student.42vienna.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/20 14:52:11 by dstumpf           #+#    #+#             */
-/*   Updated: 2026/05/06 11:00:27 by knajmech         ###   ########.fr       */
+/*   Updated: 2026/05/06 13:59:31 by knajmech         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MAIN_H
 # define MAIN_H
 
-# include "../env.h"
-
+# include "env.h"
+# include "structs.h"
+/*
 typedef struct s_data
 {
 //	t_ast	*ast;
@@ -23,5 +24,5 @@ typedef struct s_data
 	char			**new_variable;
 	t_env_tracker	*env_mp;
 }			t_data;
-
+*/
 #endif
