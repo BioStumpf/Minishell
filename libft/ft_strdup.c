cf60250a5f7e38e6d5cb3d6d1d6aa9e33261de49
Kian Najmechi <knajmech@c3r2p3.42vienna.com>
2026-05-08 09:17:54 +0200

    created hash_key creation function for streamlining. Also protect ft_strdup again null inputs

--- libft/ft_strdup.c
+++ libft/ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: dstumpf <dstumpf@student.42vienna.com      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/03 14:46:58 by dstumpf           #+#    #+#             */
-/*   Updated: 2025/10/03 16:36:38 by dstumpf          ###   ########.fr       */
+/*   Updated: 2026/05/08 09:15:36 by knajmech         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ char	*ft_strdup(const char *s)
 	size_t	len;
 	char	*out;
 
+	if (!s)
+		return (NULL);
 	len = ft_strlen(s) + 1;
 	out = malloc(len);
 	if (!out)
--- new_variables.c
+++ new_variables.c
@@ -6,22 +6,19 @@
 /*   By: knajmech <knajmech@student.42vienna.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/06 11:01:30 by knajmech          #+#    #+#             */
-/*   Updated: 2026/05/06 14:55:33 by knajmech         ###   ########.fr       */
+/*   Updated: 2026/05/08 09:14:57 by knajmech         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "headers/env.h"
 
 int	replace_or_add(t_list *map_env, t_env_tracker *tracker,
-		unsigned int hash, char **env_var)
+		unsigned int key_index, char **env_var)
 {
-	unsigned int		key_index;
 	t_env				*key_val_node;
 	t_node				*content_node;
 	char				*value[2];
 
-	assert(hash > 0);
-	key_index = hash % tracker->capacity;
 	assert(key_index <= (unsigned int) tracker->capacity);
 	value[1] = ft_strdup(env_var[0]);
 	value[0] = ft_strdup(env_var[1]);
@@ -41,19 +38,12 @@ int	replace_or_add(t_list *map_env, t_env_tracker *tracker,
 
 int	insert_new(t_list *env_ptr, t_env_tracker *tracker, char **new_variable)
 {
-	int				i_str;
 	int				var_len;
 	unsigned int	hash;
 
 	assert(new_variable != NULL && new_variable[0]);
-	i_str = 0;
-	hash = 5381;
 	var_len = ft_strlen(new_variable[0]);
-	while(i_str < var_len)//remove var_len and just check up until hardcoded '='
-	{
-		hash = hash * 31 + new_variable[0][i_str];
-		i_str++;
-	}
+	hash = find_hash_key(new_variable[0]);
 	if (var_len > 0)
 		if(replace_or_add(env_ptr, tracker, hash, new_variable) == 0)
 			return (0); //consider if variable adding is needed.
