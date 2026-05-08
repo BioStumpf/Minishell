4ffa1dc3933dc8c93172192c4af676dc417c03c4
Kian Najmechi <knajmech@c3r2p3.42vienna.com>
2026-05-08 08:14:01 +0200

    adding relinking functionality. Draft version of new lstaddback which returns either the same tail or a new tail

--- list_functions/ft_nodeadd_back.c
+++ list_functions/ft_nodeadd_back.c
@@ -1,28 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstadd_back.c                                   :+:      :+:    :+:   */
+/*   ft_nodeadd_back.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dstumpf <dstumpf@student.42vienna.com      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/10 10:45:32 by dstumpf           #+#    #+#             */
-/*   Updated: 2025/10/10 17:15:37 by dstumpf          ###   ########.fr       */
+/*   Updated: 2026/05/08 08:08:59 by knajmech         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+#include "../libft/libft.h"
 
-void	ft_lstadd_back(t_list **lst, t_list *new)
+t_node	*ft_nodeadd_back(t_node **lst, t_node *new, t_node *tail)
 {
 	t_list	*last_node;
 
 	if (!lst || !new)
-		return ;
+		return (NULL);
 	last_node = ft_lstlast(*lst);
 	if (!last_node)
-	{
+	}
 		*lst = new;
-		return ;
+		return (new);
 	}
 	last_node->next = new;
+	return (tail);
 }
