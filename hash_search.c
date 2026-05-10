737bd716eb8d55c88c9fa706832bff8927274124
Kian Najmechi <knajmech@c3r2p3.42vienna.com>
2026-05-10 09:50:23 +0200

    Committing snapshot with comments

--- hash_search.c
+++ hash_search.c
@@ -6,11 +6,31 @@
 /*   By: knajmech <knajmech@student.42vienna.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/04 10:03:18 by knajmech          #+#    #+#             */
-/*   Updated: 2026/05/06 12:48:19 by knajmech         ###   ########.fr       */
+/*   Updated: 2026/05/10 09:44:17 by knajmech         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "headers/env.h"
+
+unsigned int	find_hash_key(char	*key)
+{
+	unsigned int	hash;
+	unsigned int	key_index;
+	unsigned int	i;
+	unsigned int	key_len;
+
+	assert(key != NULL);
+	hash = 5381;
+	key_len = ft_strlen_char(key, '=');
+	i = 0;
+	while (i < key_len)
+	{
+		hash = hash * 31 + key[i];
+		i++;
+	}
+	key_index = hash / CAPACITY;
+	return (key_index);
+}
 
 t_env	*hash_search(t_node *list, char *key)
 {
