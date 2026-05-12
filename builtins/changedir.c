990ad77f12171ae12da968224c0e0d35eab64f71
Kian Najmechi <knajmech@c3r2p3.42vienna.com>
2026-05-12 09:52:23 +0200

    Builtins echo, cd, env, export, pwd done

--- builtins/changedir.c
+++ builtins/changedir.c
@@ -6,13 +6,26 @@
 /*   By: knajmech <knajmech@student.42vienna.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/11 10:00:28 by knajmech          #+#    #+#             */
-/*   Updated: 2026/05/11 10:01:22 by knajmech         ###   ########.fr       */
+/*   Updated: 2026/05/11 11:26:26 by knajmech         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../headers/builtins.h"
 
-int	change_dir()
+int	change_dir(t_data *data)
 {
-	
+	char	*curdir;
+	char	*tmp;
+
+	curdir = ft_strjoin(data->cwd, "/");
+	if (!curdir)
+		error_and_cleanup(data, "malloc");
+	tmp = curdir;
+	curdir = ft_strjoin(curdir, data->newdir);
+	if (!curdir)
+		error_and_cleanup(data, "malloc");
+	free(tmp);
+	chdir(curdir);
+	data->cwd = curdir;
+	return (1);
 }
