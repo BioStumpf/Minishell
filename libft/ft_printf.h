50959f3545e85915471d05a16166e042c48330e7
David Stumpf <david-dsf@gmx.de>
2026-05-14 19:36:56 +0200

    modify printf for error printing

--- libft/ft_printf.h
+++ libft/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: dstumpf <dstumpf@student.42vienna.com      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/02 21:20:41 by dstumpf           #+#    #+#             */
-/*   Updated: 2025/11/05 15:31:45 by dstumpf          ###   ########.fr       */
+/*   Updated: 2026/05/14 19:16:46 by david            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,7 @@ int			ft_putstr(const char *str);
 int			ft_putnbr_base(t_ulong num, t_uint base_len, char base_id);
 int			ft_putnbr_signed(int num);
 int			print_pointer(t_ulong num);
-int			ft_printf(const char *str, ...);
+int			ft_printf(int fd, const char *str, ...);
+int			gfd(void);
 
 #endif
--- src/error_handling.c
+++ src/error_handling.c
@@ -6,12 +6,12 @@
 /*   By: david <user@student.42mail.com>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/06 21:34:36 by david             #+#    #+#             */
-/*   Updated: 2026/05/13 18:27:26 by david            ###   ########.fr       */
+/*   Updated: 2026/05/14 19:34:47 by david            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
 #include "main.h"
+#include "ft_printf.h"
 
 t_err	*fetch_error(void)
 {
@@ -35,25 +35,13 @@ int	get_and_print_error(void)
 
 	err = fetch_error();
 	if (err->status == ERR_MALLOC)
-	{
-		ft_putstr_fd("Malloc fail\n", 2);
-		return (1);
-	}
+		return (ft_printf(2, "Malloc fail\n"), 1);
 	else if (err->status == PARSE_ERR_INVALID_CHAR)
-	{
-		ft_putstr_fd("Parsing error, invalid char: ", 2);
-		write(2, &err->c, 1);
-		write(2, "\n", 1);
-		return (2);
-	}
-	else if (err->status == OK)
-		return (0);
-	// else if (err->status == PARSE_ERR_UNCLOSED_QUOTES)
+	 	return (ft_printf(2, "Parsing error, invalid char: %c\n", err->c), 2);
+	else if (err->status == PARSE_ERR_UNCLOSED_QUOTES)
+	 	return (ft_printf(2, "Parsing error, unclosed quotes\n"), 2);
 	else
-	{
-		ft_putstr_fd("Parsing error, unclose quotes: ", 2);
-		return (2);
-	}
+		return (0);
 }
 
 enum e_err	evaluate_state(void)
