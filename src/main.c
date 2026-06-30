c70b40c3863ae413b4bca4293103c2bdebe79ef9
David <david-dsf@gmx.de>
2026-06-30 16:09:43 +0200

    changed main for testing

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: dstumpf <dstumpf@student.42vienna.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/04 11:02:46 by dstumpf           #+#    #+#             */
-/*   Updated: 2026/06/30 10:11:20 by david            ###   ########.fr       */
+/*   Updated: 2026/06/30 16:01:45 by david            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,10 +20,15 @@
 
 void	free_all(t_data *dat)
 {
-	rl_clear_history();
+	// rl_clear_history();
 	(void)dat;
+	// error_and_cleanup(dat, NULL, 0);
 }
 
+//NOTES:
+//1. env_value accessor needs to take care of null input (what if env is NULL?)
+//2. i added checks into env accessor for NULLs
+//3. env leaks
 int main(int argc, char **argv, char **envp)
 {
 	t_data	dat;
@@ -37,16 +42,16 @@ int main(int argc, char **argv, char **envp)
 	while (1)
 	{
 		set_error(&dat, OK);
-		dat.input = readline("minishell$ "); //here ensure that it does not always print minishell but also tha path right???? I am unsure though) i.e. instead of minishell as argument for readline use envp's pwd concatenated with minishell
+		dat.input = "\"Hi\"";
+		// dat.input = readline("minishell$ "); //here ensure that it does not always print minishell but also tha path right???? I am unsure though) i.e. instead of minishell as argument for readline use envp's pwd concatenated with minishell
 		if (!dat.input)
 			set_error(&dat, ERR_MALLOC);
 		parse_input(&dat);
-		//execute_input(dat) //kian part
+		//execute_input(&dat) //kian part
 		//clean_ast(dat.ast); //since we run infinetly, clean up the ast after each loop iteration
-		free(dat.input);
-		//set_last_return(&dat); //this is to fetch dat->return and set envp $? to this return
+		// free(dat.input);
 		return (free_all(&dat), 1);
 		// if (fatal_error(&dat))
-			// return (free_all(&dat), 1);
+		// 	return (free_all(&dat), 1);
 	}
 }
