cc3e283f333ca952f7995028610b34a8c3131ecf
KikiPowa204 <Knajmechi@gmail.com>
2026-07-13 11:49:36 +0200

    Change ft_printf function signature to include fd

--- libft/ft_printf.h
+++ libft/ft_printf.h
@@ -6,11 +6,7 @@
 /*   By: dstumpf <dstumpf@student.42vienna.com      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/02 21:20:41 by dstumpf           #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2025/11/05 15:31:45 by dstumpf          ###   ########.fr       */
-=======
-/*   Updated: 2026/05/14 19:16:46 by david            ###   ########.fr       */
->>>>>>> 9416c658e3301417b663f3d8b7ce3077880516fc
+/*   Updated: 2026/07/13 10:45:33 by knajmech         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,11 +27,7 @@ int			ft_putstr(const char *str);
 int			ft_putnbr_base(t_ulong num, t_uint base_len, char base_id);
 int			ft_putnbr_signed(int num);
 int			print_pointer(t_ulong num);
-<<<<<<< HEAD
-int			ft_printf(const char *str, ...);
-=======
 int			ft_printf(int fd, const char *str, ...);
 int			gfd(void);
->>>>>>> 9416c658e3301417b663f3d8b7ce3077880516fc
 
 #endif
