574004b3844a7443dad684936d4c60508c38bd50
David Stumpf <david-dsf@gmx.de>
2026-07-29 19:19:29 +0200

    add tmp flag for open() call so the heredoc is never actually created

--- src/expansion/heredoc.c
+++ src/expansion/heredoc.c
@@ -6,10 +6,11 @@
 /*   By: dstumpf <dstumpf@student.42vienna.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/28 10:20:35 by dstumpf           #+#    #+#             */
-/*   Updated: 2026/07/28 15:48:21 by dstumpf          ###   ########.fr       */
+/*   Updated: 2026/07/29 19:15:09 by dstumpf          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#define _GNU_SOURCE
 #include "err.h"
 #include "ft_printf.h"
 #include "libft.h"
@@ -19,15 +20,6 @@
 #include <fcntl.h>
 #include <errno.h>
 
-// void	make_name(char name[25], int id)
-// {
-// 	char	num[11];
-// 	if (id == 0)
-// 		ft_strlcpy(name, "/tmp/.heredoc_", 15);
-// 	ft_itoa_buff(id, num, 11);
-// 	ft_strlcpy(name + 14, num, 11);
-// }
-//
 static bool	read_input(t_data *dat, char **line)
 {
 	errno = 0;
@@ -72,12 +64,26 @@ void	close_heredocs(t_data *dat)
 		close(start++);
 }
 
-static bool	open_heredoc(t_data *dat, t_compound *comp)
+static void	make_name(char name[24], int fd)
 {
-	comp->u_value.s_redir.open_fd
-		= open(".heredoc", O_CREAT | O_RDWR | O_EXCL, 0600);
+	char	num[11];
+
+	ft_strlcpy(name, "/proc/self/fd/", 15);
+	ft_itoa_buff(fd, num, 11);
+	ft_strlcpy(name + 14, num, 11);
+}
+
+static bool	open_heredoc(t_data *dat, t_compound *comp, int *fd)
+{
+	char	proc_name[25];
+
+	*fd = open("/tmp", O_TMPFILE | O_WRONLY | O_EXCL, 0600);
+	if (*fd)
+		return (set_error(dat, ERR_OPEN), false);
+	make_name(proc_name, *fd);
+	comp->u_value.s_redir.open_fd = open(proc_name, O_RDONLY);
 	if (!comp_heredoc(comp))
-		return (set_error(dat, ERR_OPEN), close_heredocs(dat), false);
+		return (close(*fd), set_error(dat, ERR_OPEN), false);
 	if (dat->heredoc_start == 0)
 		dat->heredoc_start = comp_heredoc(comp);
 	dat->heredoc_end = comp_heredoc(comp);
@@ -86,28 +92,29 @@ static bool	open_heredoc(t_data *dat, t_compound *comp)
 
 bool	heredoc(t_data *dat, t_compound *comp, bool expand)
 {
+	int			fd;
 	char		*line;
 	char		*expanded;
 	t_exp_vec	exps;
 
-	if (!open_heredoc(dat, comp))
-		return (false);
+	if (!open_heredoc(dat, comp, &fd))
+		return (close_heredocs(dat), false);
 	while (1)
 	{
 		if (!read_input(dat, &line))
-			return (unlink(".heredoc"), close_heredocs(dat), false);
+			return (close_heredocs(dat), false);
 		if (find_limiter(line, comp_filename(comp)))
-			return (unlink(".heredoc"), free(line), true);
+			return (free(line), true);
 		if (expand)
 		{
 			expanded = expand_str(dat, line, KEEP_QUOTES, &exps);
 			if (!expanded)
-				return (unlink(".heredoc"), close_heredocs(dat), false);
+				return (close_heredocs(dat), false);
 			free(line);
 			free(exps.expansions);
 			line = expanded;
 		}
-		ft_putstr_fd(line, comp_heredoc(comp));
+		ft_putstr_fd(line, fd);
 		free(line);
 	}
 }
