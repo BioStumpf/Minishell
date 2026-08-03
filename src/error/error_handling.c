3b7cd093161e0851ff98921208076a7d786daa5d
David Stumpf <david-dsf@gmx.de>
2026-08-03 12:47:11 +0200

    fix error handling in ast

--- src/error/error_handling.c
+++ src/error/error_handling.c
@@ -13,20 +13,31 @@
 #include "structs.h"
 #include "ft_printf.h"
 #include "err.h"
-#include "readline_sigs.h"
 
-static int	token_error(enum e_token err)
+static int	parser_err(enum e_err err)
 {
-	if (err == AND)
+	if (err == PARSE_ERR_AND)
 		return (ft_printf(2, "Parsing error near '&&'\n"), 2);
-	else if (err == PIPE)
+	else if (err == PARSE_ERR_UNCLOSED_QUOTES)
+		return (ft_printf(2, "Parsing error, unclosed quotes\n"), 2);
+	else if (err == PARSE_ERR_REDIR)
+		return (ft_printf(2, "Parsing error, redirection invalid\n"), 2);
+	else if (err == PARSE_ERR_PIPE)
 		return (ft_printf(2, "Parsing error near '|'\n"), 2);
-	else if (err == LEFT_PARA)
+	else if (err == PARSE_ERR_LEFT_PARA)
 		return (ft_printf(2, "Parsing error near '('\n"), 2);
-	else if (err == RIGHT_PARA)
+	else if (err == PARSE_ERR_RIGHT_PARA)
 		return (ft_printf(2, "Parsing error near ')'\n"), 2);
-	else if (err == OR)
+	else if (err == PARSE_ERR_OR)
 		return (ft_printf(2, "Parsing error near '||'\n"), 2);
+	else if (err == PARSE_ERR_REDIR_OUTFILE)
+		return (ft_printf(2, "Parsing error near '>'\n"), 2);
+	else if (err == PARSE_ERR_REDIR_INFILE)
+		return (ft_printf(2, "Parsing error near '<'\n"), 2);
+	else if (err == PARSE_ERR_REDIR_APPEND)
+		return (ft_printf(2, "Parsing error near '>>'\n"), 2);
+	else if (err == PARSE_ERR_REDIR_HEREDOC)
+		return (ft_printf(2, "Parsing error near '<<'\n"), 2);
 	return (0);
 }
 
@@ -34,10 +45,6 @@ static int	print_error_get_return(enum e_err err)
 {
 	if (err == ERR_MALLOC)
 		return (ft_printf(2, "Malloc fail\n"), 1);
-	else if (err == PARSE_ERR_UNCLOSED_QUOTES)
-		return (ft_printf(2, "Parsing error, unclosed quotes\n"), 2);
-	else if (err == PARSE_ERR_REDIR)
-		return (ft_printf(2, "Parsing error, redirection invalid\n"), 2);
 	else if (err == ERR_DUP)
 		return (ft_printf(2, "dup error\n"), 1);
 	else if (err == ERR_PIPE)
@@ -53,7 +60,7 @@ static int	print_error_get_return(enum e_err err)
 	else if (err == EXIT_CALL)
 		return (0);
 	else
-		return (token_error((enum e_token)err));
+		return (parser_err(err));
 }
 
 void	set_error(t_data *dat, enum e_err status)
