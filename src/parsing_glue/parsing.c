9090fcdbc4a94c9db5bda32abb14bdd52c3e3490
Kian Najmechi <knajmech@c1r2p6.42vienna.com>
2026-08-03 11:08:34 +0200

    removed commits from parsing.c for norm

--- src/parsing_glue/parsing.c
+++ src/parsing_glue/parsing.c
@@ -6,7 +6,7 @@
 /*   By: dstumpf <dstumpf@student.42vienna.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/20 14:14:19 by dstumpf           #+#    #+#             */
-/*   Updated: 2026/08/03 10:53:40 by knajmech         ###   ########.fr       */
+/*   Updated: 2026/08/03 11:08:19 by knajmech         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,13 +23,10 @@ void	parse_input(t_data *dat)
 		return ;
 	tokens = tokenize(dat);
 	compound_tokens = compound_group(dat, tokens);
-	// print_compound(compound_tokens);
 	ft_lstclear(tokens, free_token);
 	expand(dat, compound_tokens);
-	// print_compound(compound_tokens);
 	built_ast(dat, compound_tokens);
 	free_compound(compound_tokens, OK, dat);
-	print_tree(dat->ast.root, 0);
 }
 
 	//print_tree(dat->ast.root, 0);
