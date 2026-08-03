316834fb0d1ed201bd690903365189a28bc07d61
David Stumpf <david-dsf@gmx.de>
2026-08-03 17:55:13 +0200

    start fixing return codes; todo: external commands when not found/not executable; todo: builtins need to set g_ret instead of returning values

--- src/cmd/pipe.c
+++ src/cmd/pipe.c
@@ -6,46 +6,50 @@
 /*   By: knajmech <knajmech@student.42vienna.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/12 12:08:14 by knajmech          #+#    #+#             */
-/*   Updated: 2026/07/30 11:15:56 by knajmech         ###   ########.fr       */
+/*   Updated: 2026/08/03 16:23:56 by dstumpf          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "structs.h"
 #include "execution.h"
-#include "env.h"
 #include "err.h"
+#include "parsing.h"
 
-int	launch_childp(t_ast *direction, int *fds, int std_fd,
+static void	launch_childp(t_ast *direction, int *fds, int std_fd,
 		t_pipe_manager *pipe_info)
 {
 	if (std_fd == STDOUT_FILENO)
 	{
 		if (pipe_info->child[0] == -1)
-			return (set_error(pipe_info->data, ERR_FORK), -1);
+			return (set_error(pipe_info->data, ERR_FORK));
 		else if (pipe_info->child[0] == 0)
 			close(fds[0]);
-		pipe_info->in_pipeline = IN_PIPELINE;
 	}
 	else
 	{
 		if (pipe_info->child[1] == -1)
-			return (set_error(pipe_info->data, ERR_FORK), -1);
+			return (set_error(pipe_info->data, ERR_FORK));
 		else if (pipe_info->child[1] == 0)
 			close(fds[1]);
-		pipe_info->in_pipeline = IN_PIPELINE;
 	}
+	pipe_info->in_pipeline = IN_PIPELINE;
 	if (dup2(fds[std_fd], std_fd) == -1)
 		set_error(pipe_info->data, ERR_DUP);
 	close(fds[std_fd]);
 	if (fatal_error(pipe_info->data))
-		return (-1);
+	{
+		close_heredocs(pipe_info->data);
+		cleanup_child(pipe_info->data, pipe_info);
+	}
 	return (execute(direction, pipe_info));
 }
 
-int	exec_pipe(t_ast *node, t_pipe_manager *pipe_info)
+void	exec_pipe(t_ast *node, t_pipe_manager *pipe_info)
 {
+	int		status;
 	int		fds[2];
 
+	status = 0;
 	if (pipe(fds) == -1)
 		set_error(pipe_info->data, ERR_PIPE);
 	pipe_info->child[0] = fork();
@@ -53,19 +57,17 @@ int	exec_pipe(t_ast *node, t_pipe_manager *pipe_info)
 		launch_childp(node->left, fds, STDOUT_FILENO, pipe_info);
 	pipe_info->child[1] = fork();
 	if (pipe_info->child[1] <= 0)
-	{
-		g_ret = launch_childp(node->right, fds, STDIN_FILENO, pipe_info);
-		cleanup_child(pipe_info->data, pipe_info);
-	}
-	pipe_info->data->pipe_info = pipe_info;
+		launch_childp(node->right, fds, STDIN_FILENO, pipe_info);
 	close_fds(fds, 2);
-	waitpid(pipe_info->child[0], NULL, 0);
-	waitpid(pipe_info->child[1], (int *)&g_ret, 0);
+	if (pipe_info->child[0] != -1)
+		waitpid(pipe_info->child[0], NULL, 0);
+	if (pipe_info->child[1] != -1)
+		waitpid(pipe_info->child[1], &status, 0);
+	if (WIFEXITED(status))
+		g_ret = WEXITSTATUS(status);
+	else if (WIFSIGNALED(status))
+		g_ret = 128 + WTERMSIG(status);
 	if (pipe_info->in_pipeline)
-		cleanup_child(pipe_info->data, pipe_info);
-	if (WIFEXITED(g_ret))
-		return (WEXITSTATUS(g_ret));
-	else if (WIFSIGNALED(g_ret))
-		return (128 + WTERMSIG(g_ret));
-	return (-1);
+		return (close_heredocs(pipe_info->data),
+				cleanup_child(pipe_info->data, pipe_info));
 }
